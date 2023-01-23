3adc77fd662e5fcbb0fdbb36cad54057de7a188f
MikaelahJ <mikaelah.jenkins@outlook.com>
2023-01-23 13:16:47 +0100

    Added firebase

---
+++ Assets/Scripts/FirebaseTest.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Extensions;
+
+public class FirebaseTest : MonoBehaviour
+{
+    FirebaseAuth auth;
+
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+                Debug.LogError(task.Exception);
+
+            auth = FirebaseAuth.DefaultInstance;
+        });
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+        }
+            AnonymousSignIn();
+
+        if (Input.GetKeyDown(KeyCode.D))
+            DataTest(auth.CurrentUser.UserId, Random.Range(0, 100).ToString());
+    }
+
+    private void AnonymousSignIn()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWithOnMainThread(task => {
+            if (task.Exception != null)
+            {
+                Debug.LogWarning(task.Exception);
+            }
+            else
+            {
+                FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            }
+        });
+    }
+
+    private void DataTest(string userID, string data)
+    {
+        Debug.Log("Trying to write data...");
+        var db = FirebaseDatabase.DefaultInstance;
+        db.RootReference.Child("users").Child(userID).SetValueAsync(data).ContinueWithOnMainThread(task =>
+        {
+            if (task.Exception != null)
+                Debug.LogWarning(task.Exception);
+            else
+                Debug.Log("DataTestWrite: Complete");
+        });
+    }
+}
